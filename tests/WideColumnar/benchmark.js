d2468022d8b929a57aa061957fa7356bd7d1a9a4
USERSATOSHI <user.satoshi69@gmail.com>
2023-12-09 23:06:40 +0530

    updated benchmark

--- tests/WideColumnar/benchmark.js
+++ tests/WideColumnar/benchmark.js
@@ -34,7 +34,7 @@ const wait = async (ms) => await st(ms);
 const methods = ["set", "get", "delete", "has", "all", "findOne", "findMany"];
 db.on(DatabaseEvents.Connect, async () => {
     console.log("Connected to database");
-    await wait(10000);
+    await wait(1000);
     await run();
 });
 db.connect();
@@ -46,11 +46,11 @@ async function measureOPS(fn, times) {
     }
     const end = performance.now();
     const time = end - start;
-    const ops = times / time;
-    return { time, ops: ops * 10000 , tpo: time / times };
+    const ops = (times * 1000) / time;
+    return { time, ops: ops, tpo: time / times };
 }
 
-async function set10k() {
+async function setFunc() {
     const fn = async () =>
         await db.set(
             "main",
@@ -63,61 +63,61 @@ async function set10k() {
                 value: Math.random(),
             },
         );
-    return await measureOPS(fn, 10000);
+    return await measureOPS(fn, 500);
 }
 
-async function get10k() {
+async function getFunc() {
     const fn = async () => await db.get("main", "key", Math.random());
-    return await measureOPS(fn, 10000);
+    return await measureOPS(fn, 500);
 }
 
-async function delete10k() {
+async function deleteFunc() {
     const fn = async () => await db.delete("main", "key", Math.random());
-    return await measureOPS(fn, 10000);
+    return await measureOPS(fn, 500);
 }
 
-async function has10k() {
+async function hasFunc() {
     const fn = async () => await db.has("main", "key", Math.random());
-    return await measureOPS(fn, 10000);
+    return await measureOPS(fn, 500);
 }
 
-async function all10k() {
+async function allFunc() {
     const fn = async () => await db.all("main", "key", () => true, 10);
-    return await measureOPS(fn, 10000);
+    return await measureOPS(fn, 500);
 }
 
-async function findOne10k() {
+async function findOneFunc() {
     const fn = async () => await db.findOne("main", "key", () => true);
-    return await measureOPS(fn, 10000);
+    return await measureOPS(fn, 500);
 }
 
-async function findMany10k() {
+async function findManyFunc() {
     const fn = async () => await db.findMany("main", "key", () => true);
-    return await measureOPS(fn, 10000);
+    return await measureOPS(fn, 500);
 }
 
 async function run() {
     const results = [];
-    const cycles = 1;
+    const cycles = 10;
     console.log("Running Suite...");
 
     for (let i = 0; i < cycles; i++) {
         console.log(`Cycle ${i + 1} of ${cycles}`);
-        const set = await set10k();
-        console.log("Added 10k rows to database");
-        const get = await get10k();
-        console.log("Got 10k rows from database");
-        const all = await all10k();
-        console.log("All 10k rows from database");
-        const del = await delete10k();
-        console.log("Deleted 10k rows from database");
-        const has = await has10k();
-        console.log("Checked 10k rows from database");
+        const set = await setFunc();
+        // console.log("Added 10k rows to database");
+        const get = await getFunc();
+        // console.log("Got 10k rows from database");
+        const all = await allFunc();
+        // console.log("All 10k rows from database");
+        const del = await deleteFunc();
+        // console.log("Deleted 10k rows from database");
+        const has = await hasFunc();
+        // console.log("Checked 10k rows from database");
 
-        const findOne = await findOne10k();
-        console.log("Found 10k rows from database");
-        const findMany = await findMany10k();
-        console.log("Found 10k rows from database");
+        const findOne = await findOneFunc();
+        // console.log("Found 10k rows from database");
+        const findMany = await findManyFunc();
+        // console.log("Found 10k rows from database");
 
         results.push({
             set,
@@ -139,7 +139,7 @@ async function run() {
         ops: Number(ops(results.map((a) => a.set.ops)).toFixed(0)),
         max: Number(max(results.map((a) => a.set.ops)).toFixed(0)),
         min: Number(min(results.map((a) => a.set.ops)).toFixed(0)),
-        tpo: Number(ops(results.map((a) => a.set.tpo)).toFixed(0)),
+        tpo: Number(ops(results.map((a) => a.set.tpo))),
     };
 
     const get = {
@@ -147,7 +147,7 @@ async function run() {
         ops: Number(ops(results.map((a) => a.get.ops)).toFixed(0)),
         max: Number(max(results.map((a) => a.get.ops)).toFixed(0)),
         min: Number(min(results.map((a) => a.get.ops)).toFixed(0)),
-        tpo: Number(ops(results.map((a) => a.get.tpo)).toFixed(0)),
+        tpo: Number(ops(results.map((a) => a.get.tpo))),
     };
 
     const del = {
@@ -155,7 +155,7 @@ async function run() {
         ops: Number(ops(results.map((a) => a.del.ops)).toFixed(0)),
         max: Number(max(results.map((a) => a.del.ops)).toFixed(0)),
         min: Number(min(results.map((a) => a.del.ops)).toFixed(0)),
-        tpo: Number(ops(results.map((a) => a.del.tpo)).toFixed(0)),
+        tpo: Number(ops(results.map((a) => a.del.tpo))),
     };
 
     const has = {
@@ -163,7 +163,7 @@ async function run() {
         ops: Number(ops(results.map((a) => a.has.ops)).toFixed(0)),
         max: Number(max(results.map((a) => a.has.ops)).toFixed(0)),
         min: Number(min(results.map((a) => a.has.ops)).toFixed(0)),
-        tpo: Number(ops(results.map((a) => a.has.tpo)).toFixed(0)),
+        tpo: Number(ops(results.map((a) => a.has.tpo))),
     };
 
     const all = {
@@ -171,7 +171,7 @@ async function run() {
         ops: Number(ops(results.map((a) => a.all.ops)).toFixed(0)),
         max: Number(max(results.map((a) => a.all.ops)).toFixed(0)),
         min: Number(min(results.map((a) => a.all.ops)).toFixed(0)),
-        tpo: Number(ops(results.map((a) => a.all.tpo)).toFixed(0)),
+        tpo: Number(ops(results.map((a) => a.all.tpo))),
     };
 
     const findOne = {
@@ -179,7 +179,7 @@ async function run() {
         ops: Number(ops(results.map((a) => a.findOne.ops)).toFixed(0)),
         max: Number(max(results.map((a) => a.findOne.ops)).toFixed(0)),
         min: Number(min(results.map((a) => a.findOne.ops)).toFixed(0)),
-        tpo: Number(ops(results.map((a) => a.findOne.tpo)).toFixed(0)),
+        tpo: Number(ops(results.map((a) => a.findOne.tpo))),
     };
 
     const findMany = {
@@ -187,7 +187,7 @@ async function run() {
         ops: Number(ops(results.map((a) => a.findMany.ops)).toFixed(0)),
         max: Number(max(results.map((a) => a.findMany.ops)).toFixed(0)),
         min: Number(min(results.map((a) => a.findMany.ops)).toFixed(0)),
-        tpo: Number(ops(results.map((a) => a.findMany.tpo)).toFixed(0)),
+        tpo: Number(ops(results.map((a) => a.findMany.tpo))),
     };
 
     const arr = {
