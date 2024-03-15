72d31269f4bafdd8abd4aa1c6cca37cb722c408c
USERSATOSHI <user.satoshi69@gmail.com>
2024-03-15 10:44:35 +0530

    rebuilt dist

--- dist/esm/Columnar/src/Column.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=Column.d.ts.map
--- dist/esm/Columnar/typings/types.d.ts
+++
@@ -1,3 +0,0 @@
-export type ColumnType = "string" | "number" | "boolean" | "object" | "null" | "undefined" | "bigint" | "date";
-export type ColumnDataType = string | number | boolean | object | null | undefined | bigint | Date;
-//# sourceMappingURL=types.d.ts.map
